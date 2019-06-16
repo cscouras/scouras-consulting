2261508c22b0cf01e1c3961137c534d18b0468b9
Chris Scouras <chris.scouras@gmail.com>
2019-06-15 20:13:23 -0700

    Update footer styling and mobile responsiveness

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,25 +3,28 @@ import { Link } from 'gatsby'
 import styles from '../styles/footer.module.css'
 
 const Footer = props => (
-    <div className={styles.container}>
-    <div className={styles.footerCategory}>
-            <span className={styles.categoryHeader}>IFP Apps</span>
-            <Link to="/rsicustom/" className={styles.link}>RSI Custom</Link>
-            <Link to="/multivariate/" className={styles.link}>MVB Integrator</Link>
-            <Link to="/techextend/" className={styles.link}>TechExtend</Link>
-        </div>
-        <div className={styles.footerCategory}>
-            <span className={styles.categoryHeader}>Mobile App</span>
-            <Link to="/mobile/" className={styles.link}>ImagineMobile</Link>
-            <Link to="/mobilepromo/" className={styles.link}>Overview</Link>
-            <Link to="/mobileinstallation/" className={styles.link}>Installation</Link>
-            <Link to="/mobileconfiguration/" className={styles.link}>Configuration</Link>
-            <Link to="/mobilenavigation/" className={styles.link}>Navigation</Link>
-        </div>
-        <div className={styles.footerCategory}>
-            <span className={styles.categoryHeader}>Company</span>
-            <Link to="/about/" className={styles.link}>About</Link>
-            <Link to="/contact/" className={styles.link}>Contact</Link>
+    <div className={styles.footer}>
+        <div className={styles.categoryContainer}>
+            <div className={styles.footerCategory}>
+                <span className={styles.categoryHeader}>IFP Apps</span>
+                <Link to="/rsicustom/" className={styles.link}>RSI Custom</Link>
+                <Link to="/multivariate/" className={styles.link}>MVB Integrator</Link>
+                <Link to="/techextend/" className={styles.link}>TechExtend</Link>
+            </div>
+            <div className={styles.footerCategory}>
+                <span className={styles.categoryHeader}>Mobile App</span>
+                <Link to="/mobile/" className={styles.link}>ImagineMobile</Link>
+                <Link to="/mobilepromo/" className={styles.link}>Overview</Link>
+                <Link to="/mobileinstallation/" className={styles.link}>Installation</Link>
+                <Link to="/mobileconfiguration/" className={styles.link}>Configuration</Link>
+                <Link to="/mobilenavigation/" className={styles.link}>Navigation</Link>
+            </div>
+            <div className={styles.footerCategory}>
+                <span className={styles.categoryHeader}>Company</span>
+                <Link to="/about/" className={styles.link}>About</Link>
+                <Link to="/contact/" className={styles.link}>Contact</Link>
+                <Link to="/support/" className={styles.link}>Support</Link>
+            </div>
         </div>
     </div>
 )
