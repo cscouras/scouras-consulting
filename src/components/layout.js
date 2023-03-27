66440932f2113820da4eb9382bdee5ce8c8cfff8
Chris Scouras <chris.scouras@gmail.com>
2023-03-26 23:34:17 -0400

    Update to v5 (#57)
    * Replaced react-helmet with Head API
    
    * Fixed layout on 404 page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,42 +1,19 @@
 import React from 'react'
+
 import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
 import Footer from './footer'
 import './layout.css'
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render=  {data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Scouras Consulting - Specializing in Imagine IFP App Development' },
-            { name: 'keywords', content: 'Scouras, Financial Consulting, Financial Apps, ImagineMobile, RSI Custom, MVB Integrator, TechExtend' },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        <div className="site">
-        <Header siteTitle={data.site.siteMetadata.title} />
-           <div className="siteContent">{children}</div>
-        <Footer />
-        </div>
-      </>
-    )}
-  />
+  <>
+    <div className="site">
+      <Header siteTitle="Scouras Consulting" />
+      <div className="siteContent">{children}</div>
+      <Footer />
+    </div>
+  </>
 )
 
 Layout.propTypes = {
